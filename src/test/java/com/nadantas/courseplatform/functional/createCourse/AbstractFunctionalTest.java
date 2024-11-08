f48bbf71a207277380eb0e266e5069eeb1d63c17
nathaliadv <nathaliadantasv@gmail.com>
2024-11-07 21:20:31 -0300

    adding tests

---
+++ src/test/java/com/nadantas/courseplatform/functional/createCourse/AbstractFunctionalTest.java
@@ -0,0 +1,29 @@
+package com.nadantas.courseplatform.functional.createCourse;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+public abstract class AbstractFunctionalTest {
+
+    protected MockMvc mvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Before
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .build();
+    }
+}
---
+++ src/test/java/com/nadantas/courseplatform/functional/createCourse/CreateCourseFunctionalTest.java
@@ -0,0 +1,34 @@
+package com.nadantas.courseplatform.functional.createCourse;
+
+import com.nadantas.courseplatform.adapter.inbound.http.course.dto.request.CreateCourseRequestDTO;
+import com.nadantas.courseplatform.shared.Utils;
+import com.nadantas.courseplatform.shared.enums.Category;
+
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+public class CreateCourseFunctionalTest extends AbstractFunctionalTest {
+
+    @Test
+    public void shouldCreateACourse() throws Exception {
+        mvc.perform(
+                MockMvcRequestBuilders.post("/courses/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(Utils.objectToJSON(createCourseRequestDTO()))
+        ).andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(content().string(org.hamcrest.Matchers.containsString("Course registered successfully! Id: ")));
+    }
+
+    private CreateCourseRequestDTO createCourseRequestDTO() {
+        return CreateCourseRequestDTO.builder()
+                .name("Name Test Course")
+                .category(Category.BACKEND)
+                .description("Description Test Course")
+                .active(true)
+                .build();
+    }
+}
